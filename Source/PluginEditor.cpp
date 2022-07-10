97ecd70b608693f11abc56debd3f663001a5b9b0
juyejilala <108221579+juyejilala@users.noreply.github.com>
2022-07-10 20:35:50 +0800

    Update PluginEditor.cpp

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,5 +1,6 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
+#include <algorithm>
 
 CREAMProjectAudioProcessorEditor::CREAMProjectAudioProcessorEditor (CREAMProjectAudioProcessor& p)
 : AudioProcessorEditor (&p)
