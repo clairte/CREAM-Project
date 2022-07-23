1eee9f5916efe19c409989f2995925dd194a8b71
Claire Xu <claireaxu@g.ucla.edu>
2022-07-23 15:06:17 +0800

    Update components

--- Source/UI/FilterComponent.cpp
+++ Source/UI/FilterComponent.cpp
@@ -1,71 +1,33 @@
 #include <JuceHeader.h>
 #include "FilterComponent.h"
 
-//==============================================================================
 FilterComponent::FilterComponent(juce::AudioProcessorValueTreeState& apvts, juce::String filterTypeSelectorId, juce::String filterCutoffId, juce::String filterResId)
+: cutoff("Cutoff", filterCutoffId, apvts, dialWidth, dialHeight)
+, resonance("Resonance", filterResId, apvts, dialWidth, dialHeight)
 {
-    juce::StringArray choices {"Low-Pass", "Band-Pass", "High-Pass"};
-    filterTypeSelector.addItemList(choices, 1);
+    juce::StringArray filterTypeChoices {"Low-Pass", "Band-Pass", "High-Pass"};
+    filterTypeSelector.addItemList(filterTypeChoices, 1);
+    filterTypeSelector.setSelectedItemIndex(0);
     addAndMakeVisible(filterTypeSelector);
     
     filterTypeSelectorAttachment = std::make_unique<juce::AudioProcessorValueTreeState::ComboBoxAttachment>(apvts, filterTypeSelectorId, filterTypeSelector);
     
-    filterSelectorLabel.setColour (juce::Label::ColourIds::textColourId, juce::Colours::white);
-    filterSelectorLabel.setFont (15.0f);
-    filterSelectorLabel.setJustificationType (juce::Justification::left);
-    addAndMakeVisible (filterSelectorLabel);
-    
-    setSliderWithLabel(filterCutoffSlider, filterCutoffLabel, apvts, filterCutoffId, filterCutoffAttachment);
-    setSliderWithLabel(filterResSlider, filterResLabel, apvts, filterResId, filterResAttachment);
+    addAndMakeVisible(cutoff);
+    addAndMakeVisible(resonance);
 }
 
 FilterComponent::~FilterComponent()
 {
 }
 
-void FilterComponent::paint (juce::Graphics& g)
-{
-    auto bounds = getLocalBounds().reduced (5);
-    auto labelSpace = bounds.removeFromTop (25.0f);
-        
-    g.fillAll (juce::Colours::black);
-    g.setColour (juce::Colours::white);
-    g.setFont (20.0f);
-    g.drawText ("Filter", labelSpace.withX (5), juce::Justification::left);
-    g.drawRoundedRectangle (bounds.toFloat(), 5.0f, 2.0f);
-}
-
 void FilterComponent::resized()
 {
-    const int startY = 55;
-    const int sliderWidth = 100;
-    const int sliderHeight = 90;
-    const int labelYOffset = 20;
-    const int labelHeight = 20;
+    const int startX = 18;
+    const int startY = 80;
+    const int width = 70;
+    const int height = 88;
     
-    filterTypeSelector.setBounds(10, startY + 5, 90, 30);
-    filterSelectorLabel.setBounds(10, startY - labelYOffset, 90, labelHeight);
-    
-    filterCutoffSlider.setBounds(filterTypeSelector.getRight(), startY, sliderWidth, sliderHeight);
-    filterCutoffLabel.setBounds(filterCutoffSlider.getX(), filterCutoffSlider.getY() - labelYOffset, filterCutoffSlider.getWidth(), labelHeight);
-    
-    filterResSlider.setBounds(filterCutoffSlider.getRight(), startY, sliderWidth, sliderHeight);
-    filterResLabel.setBounds(filterResSlider.getX(), filterResSlider.getY() - labelYOffset, filterResSlider.getWidth(), labelHeight);
-}
-
-using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
-
-void FilterComponent::setSliderWithLabel (juce::Slider& slider, juce::Label& label, juce::AudioProcessorValueTreeState& apvts, juce::String paramId, std::unique_ptr<Attachment>& attachment)
-{
-    slider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
-    slider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 50, 25);
-    addAndMakeVisible(slider);
-    
-    attachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(apvts, paramId, slider);
-    
-    label.setColour(juce::Label::ColourIds::textColourId, juce::Colours::white);
-    label.setJustificationType(juce::Justification::centred);
-    label.setFont(15.0f);
-    addAndMakeVisible(label);
-    
+    filterTypeSelector.setBounds(18, 40, 145, 25);
+    cutoff.setBounds(startX, startY, width, height);
+    resonance.setBounds(cutoff.getRight(), startY, width, height);
 }
--- Source/UI/FilterComponent.h
+++ Source/UI/FilterComponent.h
@@ -1,33 +1,25 @@
 #pragma once
 
 #include <JuceHeader.h>
+#include "UtilityComponents.h"
 
-class FilterComponent  : public juce::Component
+class FilterComponent  : public CustomComponent
 {
 public:
-    FilterComponent(juce::AudioProcessorValueTreeState& apvts, juce::String filterTypeSelectorId, juce::String filterCutoffId, juce::String filterResId);
+    FilterComponent(juce::AudioProcessorValueTreeState& apvts, juce::String filterTypeId, juce::String filterCutoffId, juce::String filterResId);
     ~FilterComponent() override;
 
-    void paint (juce::Graphics&) override;
     void resized() override;
 
 private:
     juce::ComboBox filterTypeSelector { "Filter Type" };
     std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment> filterTypeSelectorAttachment;
     
-    juce::Slider filterCutoffSlider;
-    juce::Slider filterResSlider;
+    SliderWithLabel cutoff;
+    SliderWithLabel resonance;
     
-    using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
-    
-    std::unique_ptr<Attachment> filterCutoffAttachment;
-    std::unique_ptr<Attachment> filterResAttachment;
-    
-    juce::Label filterSelectorLabel { "Filter Type", "Filter Type" };
-    juce::Label filterCutoffLabel {"Filter Freq", "Filter Freq"};
-    juce::Label filterResLabel {"Filter Res", "FilterRes"};
-    
-    void setSliderWithLabel (juce::Slider& slider, juce::Label& label, juce::AudioProcessorValueTreeState& apvts, juce::String paramId, std::unique_ptr<Attachment>& attachment);
+    static constexpr int dialWidth = 70;
+    static constexpr int dialHeight = 70;
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FilterComponent)
 };
